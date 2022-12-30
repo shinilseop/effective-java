2ecff5e8850af59b65770ab5e6c2fe4511a0b91f
shinilseop <shinilseop12@gmail.com>
2022-12-30 15:24:20 +0900

    item5

--- src/ch1/item5_use_di.java
+++ src/ch1/item5_use_di.java
@@ -1,5 +1,7 @@
 package ch1;
 
+import java.util.Objects;
+
 public class item5_use_di {
 
     public static void main(String[] args) {
@@ -7,6 +9,12 @@ public class item5_use_di {
     }
 }
 
-class SpellChcker {
-    private final
+class SpellChecker { // 맞춤법 검사기
+    private final Lexicon dictionary;
+
+    public SpellChecker(Lexicon dictionary) {
+        this.dictionary = Objects.requireNonNull(dictionary);
+    }
 }
+
+class Lexicon {} // 각 사전별 맞춤법
