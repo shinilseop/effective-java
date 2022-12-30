0853dbe6595137707c500ea8af102dbf0e2ffb9b
shinilseop <shinilseop12@gmail.com>
2022-12-30 15:46:53 +0900

    item6

---
+++ src/ch1/item6_avoid_useless_instance.java
@@ -0,0 +1,60 @@
+package ch1;
+
+import java.time.Duration;
+import java.time.LocalTime;
+import java.util.regex.Pattern;
+
+public class item6_avoid_useless_instance {
+
+    public static void main(String[] args) {
+        final int LOOP = Integer.MAX_VALUE;
+
+        System.out.println("String instance compare");
+        String s = "123";
+        LocalTime start = LocalTime.now();
+        for (int i = 0; i < LOOP; i++) {
+            s = "123";
+        }
+        LocalTime end = LocalTime.now();
+        Duration diff = Duration.between(start, end);
+        System.out.printf("immutable nano : %d\n", diff.toNanos());
+
+        start = LocalTime.now();
+        for (int i = 0; i < LOOP; i++) {
+            s = new String("123");
+        }
+        end = LocalTime.now();
+        diff = Duration.between(start, end);
+        System.out.printf("mutable nano   : %d", diff.toNanos());
+
+        System.out.println("\n\nString Pattern match compare");
+        start = LocalTime.now();
+        for (int i = 0; i < LOOP/10000; i++) {
+            isRomanNumeralCached("II");
+        }
+        end = LocalTime.now();
+        diff = Duration.between(start, end);
+        System.out.printf("pattern cached nano : %d\n", diff.toNanos());
+
+        start = LocalTime.now();
+        for (int i = 0; i < LOOP/10000; i++) {
+            isRomanNumeralNotCached("II");
+        }
+        end = LocalTime.now();
+        diff = Duration.between(start, end);
+        System.out.printf("not cached nano     : %d", diff.toNanos());
+    }
+
+    private static final Pattern ROMAN = Pattern.compile(
+        "^(?=.)M*(C[MD]|D?C{0,3})"
+            + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+
+    static boolean isRomanNumeralCached(String s) {
+        return ROMAN.matcher(s).matches();
+    }
+
+    static boolean isRomanNumeralNotCached(String s) {
+        return s.matches("^(?=.)M*(C[MD]|D?C{0,3})"
+            + "(X[CL]|L?X{0,3})(I[XV]|V?I{0,3})$");
+    }
+}
