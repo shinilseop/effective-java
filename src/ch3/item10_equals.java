25bceb2bb9157ee797ed48aaf310a2cb78b39237
shinilseop <shinilseop12@gmail.com>
2022-12-31 22:22:08 +0900

    ch3 : item10 : equals2

--- src/ch3/item10_equals.java
+++ src/ch3/item10_equals.java
@@ -1,5 +1,9 @@
 package ch3;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
 public class item10_equals {
 
     public static void main(String[] args) {
@@ -7,9 +11,21 @@ public class item10_equals {
         Person p2 = new Person("sseop");
         Person p3 = new Person("son");
 
-        System.out.println("p1 == p2 : "+p1.equals(p2));
-        System.out.println("p1 == p3 : "+p1.equals(p3));
-        System.out.println("p2 == p3 : "+p2.equals(p3));
+        System.out.println("p1.equals(p2) : "+p1.equals(p2));
+        System.out.println("p1.equals(p3) : "+p1.equals(p3));
+        System.out.println("p2.equals(p3) : "+p2.equals(p3));
+//        NotEqualsPerson p4 = new NotEqualsPerson("son");
+//        NotEqualsPerson p5 = new NotEqualsPerson("son");
+//        System.out.println("\np4.equals(p5) : "+p4.equals(p5)); // assertion error
+
+        CaseInsensitiveString s1 = new CaseInsensitiveString("Hello");
+        String s2 = "Hello";
+        System.out.println("\ns1.equals(s2) : "+s1.equals(s2));
+        System.out.println("s2.equals(s2) : "+s2.equals(s1));
+
+        List<CaseInsensitiveString> list = new ArrayList<>();
+        list.add(s1);
+        System.out.println("list.contains(s1) : "+list.contains(s1));
     }
 }
 
@@ -23,10 +39,37 @@ class Person {
     @Override
     public boolean equals(Object o) {
         if (o instanceof Person) {
-            Person p = (Person) o;
-            return p.name == name;
+            return ((Person) o).name == name;
         }
 
+        return false;
+    }
+}
+
+class NotEqualsPerson {
+
+    String name;
+
+    public NotEqualsPerson(String name) {
+        this.name = name;
+    }
+}
+
+class CaseInsensitiveString {
+    private final String s;
+
+    public CaseInsensitiveString(String s) {
+        this.s = Objects.requireNonNull(s);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o instanceof CaseInsensitiveString) {
+            return s.equalsIgnoreCase(((CaseInsensitiveString) o).s);
+        }
+        if (o instanceof String) {
+            return s.equalsIgnoreCase((String) o);
+        }
         return false;
     }
 }
